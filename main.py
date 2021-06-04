05bf664e7c0dbdba778ec4b1fa4f2a7ee4cfd9b1
heavyrisem <pyo1748@gmail.com>
2021-06-04 10:39:14 +0900

    /ocr fix

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ async def run_ai(file: UploadFile = File(...)):
     print(type(image))
     testResult = ["코딩을 하는 한지수가 보이네요", "잠을 자는 동현이가 보이네요", "책상 위에 놓인 노트북이 보이네요", "물병 두 개가 보이네요"]
 
-
     return {"result": random.choice(testResult)}
 
 @app.post("/ocr")
@@ -41,3 +40,5 @@ async def run_ocr(file: UploadFile = File(...)):
     image = await file.read()
     print(type(image))
     testResult = ["동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세", "마하반야바라밀다심경 관자재보살 행심반야바라밀다시 조견오온개공 도일체고액 사리자"]
+
+    return {"result": random.choice(testResult)}
