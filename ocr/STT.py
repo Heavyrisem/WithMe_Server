603a18fee0420362977c70fd0f97beae43937f88
heavyrisem <pyo1748@gmail.com>
2021-08-24 15:47:12 +0900

    TTS encoding

--- ocr/STT.py
+++ ocr/STT.py
@@ -1,41 +1,47 @@
-from playsound import playsound
-playsound("O.wav")
+# from playsound import playsound
+# playsound("O.wav")
 # # Imports the Google Cloud client library
-# from google.cloud import speech
+from google.cloud import speech
 # import base64
-# import io
+import io
+import os
+import wave
 
-# # Instantiates a client
-# client = speech.SpeechClient()
-
-# with io.open('./bs', "r") as audio_file:
-#     content = audio_file.read()
-#     content = base64.b64decode((content).split(",")[1])
-
-# # with io.open('./bs', "wb") as f:
-# #     b = base64.b64decode((content).split(",")[1])
-
-# #     print(b)
-# #     f.write(b)
-
-# # print(content)
+with wave.open('../output.mp3', "rb") as wave_file:
+    frame_rate = wave_file.getframerate()
 
 
-# # The name of the audio file to transcribe
-# # gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
+    # # Instantiates a client
+    client = speech.SpeechClient()
+
+    # with io.open('./bs', "r") as audio_file:
+    #     content = audio_file.read()
+    #     content = base64.b64decode((content).split(",")[1])
+
+    # # with io.open('./bs', "wb") as f:
+    # #     b = base64.b64decode((content).split(",")[1])
+
+    # #     print(b)
+    # #     f.write(b)
+
+    # # print(content)
 
 
-# audio = speech.RecognitionAudio(content=content)
-# # audio = speech.RecognitionAudio(uri="gs://cloud-samples-data/speech/brooklyn_bridge.raw")
+    # # The name of the audio file to transcribe
+    gcs_uri = "gs://cloud-samples-data/speech/brooklyn_bridge.raw"
 
-# config = speech.RecognitionConfig(
-#     encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-#     sample_rate_hertz=48000,
-#     language_code="ko-KR",
-# )
 
-# # # Detects speech in the audio file
-# response = client.recognize(config=config, audio=audio)
-# print(response)
-# for result in response.results:
-#     print("Transcript: {}".format(result.alternatives[0].transcript))
+    audio = speech.RecognitionAudio(content=wave_file)
+    # audio = speech.RecognitionAudio(uri="gs://cloud-samples-data/speech/brooklyn_bridge.raw")
+
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=frame_rate,
+        language_code="en-US",
+    )
+
+    # # # Detects speech in the audio file
+    response = client.recognize(config=config, audio=audio)
+    print(response)
+    for result in response.results:
+        print("Transcript: {}".format(result.alternatives[0].transcript))
--- ocr/TTS.py
+++ ocr/TTS.py
@@ -22,7 +22,7 @@ def TextToBase64(text: str):
         )
 
         audio_config = texttospeech.AudioConfig(
-            audio_encoding=texttospeech.AudioEncoding.MP3
+            audio_encoding=texttospeech.AudioEncoding.LINEAR16
         )
 
         response = client.synthesize_speech(
