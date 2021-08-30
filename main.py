929bd6d148dc88444e2d035ebd539666fb906c17
heavyrisem <pyo1748@gmail.com>
2021-08-30 21:28:09 +0900

    port to 80

--- main.py
+++ main.py
@@ -120,4 +120,4 @@ async def run_ocr(file: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=3001)
+    uvicorn.run("main:app", host="0.0.0.0", port=80)
